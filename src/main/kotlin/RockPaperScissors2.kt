76a8d02b0ed1f65582da6905c8d95f8516088e7b
Andrey Peshko <andrey.peshko@zendesk.com>
2022-12-10 15:08:37 +0100

    Task 2 for solution 2

--- src/main/kotlin/RockPaperScissors2.kt
+++ src/main/kotlin/RockPaperScissors2.kt
@@ -3,7 +3,7 @@ import java.io.File
 fun main() {
     val testInput = File("src/main/kotlin/rock_paper_scissors_input.txt").readText()
 
-    fun calculate(input: String): Int {
+    fun calculate1(input: String): Int {
         var sum = 0
 
         input.split("\n").map { line ->
@@ -12,13 +12,31 @@ fun main() {
 
             sum += me
             when (enemy - me) {
-                -1, 2 -> sum += 6 //won
-                0 -> sum += 3 //draw
-                -2, 1 -> sum += 0 //loss
+                -1, 2 -> sum += 6 // won
+                0 -> sum += 3 // draw
+                -2, 1 -> sum += 0 // loss
             }
         }
         return sum
     }
 
-    println(calculate(testInput))
+    fun calculate2(input: String): Int {
+        var sum = 0
+
+        input.split("\n").map { line ->
+            val result = (line[2] - 'X') * 3
+            val enemy = line[0] - 'A' + 1
+
+            sum += result
+
+            when (result - enemy) {
+                3, 2, -2 -> sum += 1 // rock
+                5, 1, -3 -> sum += 2 // paper
+                4, 0, -1 -> sum += 3 // scissors
+            }
+        }
+        return sum
+    }
+
+    println(calculate2(testInput))
 }
