c694781d33f6391adc8e9cf0d0c99616757014cf
Andrey Peshko <andrey.peshko@zendesk.com>
2022-12-11 19:22:15 +0100

    Day 3 task 2 solution

--- src/main/kotlin/day_3/Rucksack.kt
+++ src/main/kotlin/day_3/Rucksack.kt
@@ -3,10 +3,12 @@ package day_3
 import java.io.File
 
 fun main() {
-    val testInput = File("src/main/kotlin/day_3/test_input.txt").readText().lines()
+    val task1Input = File("src/main/kotlin/day_3/test_input.txt").readText().lines()
+    val task2Input = File("src/main/kotlin/day_3/test_input_2.txt").readText().lines()
 
     fun calculate1(input: List<String>): Int {
         return input
+            .asSequence()
             .map { Pair(it.substring(0 until it.length / 2), it.substring(it.length / 2)) }
             .map { pair ->
                 pair.first.filter { pair.second.contains(it) }
@@ -16,5 +18,16 @@ fun main() {
             .sumOf { it.code }
     }
 
-    println(calculate1(testInput))
+    fun calculate2(input: List<String>): Int {
+        return input
+            .asSequence()
+            .chunked(3)
+            .map { item -> item.zipWithNext { left, right ->  left.filter { right.contains(it) }} }
+            .map { item -> item.zipWithNext { left, right ->  left.filter { right.contains(it) }} }
+            .map { it[0][0] }
+            .map { if (it.isUpperCase()) it.toLowerCase() - 'A'.code - 5 else it.toUpperCase() - 'A'.code + 1 }
+            .sumOf { it.code }
+    }
+
+    println(calculate2(task2Input))
 }
